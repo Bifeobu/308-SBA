a1be98370871902e4d14bd41053088c8205fe5a1
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-22 23:43:22 -0400

    modified declaration for learner 125

--- script.js
+++ script.js
@@ -117,10 +117,13 @@ try {
     console.log(error);
 }
 
-const learner125 = (LearnerSubmissions[0, 0] || LearnerSubmissions[1, 0] || LearnerSubmissions[2, 0])
+// const learner125 = (LearnerSubmissions[0, 0] || LearnerSubmissions[1, 0] || LearnerSubmissions[2, 0])
 
+// console.log(learner125)
+
+// const learner132 = (LearnerSubmissions[3] || LearnerSubmissions[4])
+
+// console.log(learner132)
+
+const learner125 = (LearnerSubmissions[0].learner_id)
 console.log(learner125)
-
-const learner132 = (LearnerSubmissions[3] || LearnerSubmissions[4])
-
-console.log(learner132)
